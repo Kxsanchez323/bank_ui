d3c9e568d756b3f133b66d6e545b52aff55b47b3
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2023-12-09 13:22:55 -0500

    Final commit

--- src/components/TransactionCard.js
+++ src/components/TransactionCard.js
@@ -1,10 +1,76 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Dimensions, Image } from "react-native";
+import React from "react";
 
-export default function TransactionCard() {
+const { width, height } = Dimensions.get("window");
+
+export default function TransactionCard({
+  name,
+  type,
+  amount,
+  date,
+  imageSource,
+}) {
   return (
-    <View>
-      <Text>TransactionCard</Text>
+    <View
+      className="rounded-3xl py-8 px-4 justify-between flex-row items-center bg-[#e5e5e5] shadow-sm"
+      style={{
+        width: "100%",
+        maxWidth: width,
+        height: height * 0.12,
+      }}
+    >
+      <View className="flex-row space-x-6 items-center justify-center">
+        <View className=" bg-white rounded-xl">
+          <Image
+            source={imageSource}
+            style={{
+              width: 65,
+              height: 65,
+            }}
+            resizeMode="contain"
+          />
+        </View>
+
+        <View className="space-y-1">
+          <Text
+            className="text-2xl"
+            style={{
+              fontFamily: "SpaceGroteskBold",
+            }}
+          >
+            {name}
+          </Text>
+
+          <Text
+            className="text-sm text-neutral-500"
+            style={{
+              fontFamily: "SpaceGroteskMedium",
+            }}
+          >
+            {type}
+          </Text>
+        </View>
+      </View>
+
+      <View className="space-y-1">
+        <Text
+          className="text-xl"
+          style={{
+            fontFamily: "SpaceGroteskBold",
+          }}
+        >
+          {amount}
+        </Text>
+
+        <Text
+          className="text-sm text-neutral-500"
+          style={{
+            fontFamily: "SpaceGroteskBold",
+          }}
+        >
+          {date}
+        </Text>
+      </View>
     </View>
-  )
+  );
 }
